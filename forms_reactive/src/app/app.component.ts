00da3f9c5b5b35c489f19c4e1bbc00fbecb7b0c6
HugoSimoes55 <109922698+HugoSimoes55@users.noreply.github.com>
2024-08-23 17:58:13 +0100

    Forms - Reactive Started

--- forms_reactive/src/app/app.component.ts
+++ forms_reactive/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'forms_reactive';
+export class AppComponent implements OnInit {
+	title = 'Forms Reactive';
+
+	genders = ['Male', 'Female'];
+	signupForm: FormGroup;
+
+	constructor(private titleServ: Title) {
+		this.titleServ.setTitle(this.title);
+	}
+
+	ngOnInit() {
+		this.signupForm = new FormGroup({
+			"username": new FormControl(null, Validators.required),
+			"email": new FormControl(null, [Validators.required, Validators.email]),
+			"gender": new FormControl("Male")
+		});
+	}
+
+	onSubmit() {
+		console.log(this.signupForm);
+
+	}
 }
